170f3f84711910a3c8a565f34f1c862915e3e1f6
Alvin Galit <aroblesgalit@gmail.com>
2020-08-25 18:26:18 -0700

    Created a copy of posts and sorted them

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -7,11 +7,14 @@ import { TimeAgo } from './TimeAgo';
 export const PostsList = () => {
     const posts = useSelector(state => state.posts);
 
-    const renderedPosts = posts.map(post => (
+    // Sort posts in reverse chronological order by datetime string
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
+
+    const renderedPosts = orderedPosts.map(post => (
         <article className='post-excerpt' key={post.id}>
             <h3>{post.title}</h3>
-            <TimeAgo timestam={post.date} />
             <PostAuthor userId={post.user} />
+            <TimeAgo timestamp={post.date} />
             <p>{post.content.substring(0, 100)}</p>
             <Link to={`/posts/${post.id}`} className='button muted-button'>
                 View Post
--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -23,13 +23,13 @@ export const SinglePostPage = ({ match }) => {
         <section>
             <article className='post'>
                 <h2>{post.title}</h2>
-                <TimeAgo timestamp={post.date} />
                 <PostAuthor userId={post.user} />
+                <TimeAgo timestamp={post.date} />
                 <p className='post-content'>{post.content}</p>
                 <Link to={`/editPost/${post.id}`} className='button'>
                     Edit Post
                 </Link>
             </article>
-        </section >
+        </section>
     )
 };
