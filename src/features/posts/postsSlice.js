7dc4a9a6a1b8b71423e9afcbbd71577211f00316
Alvin Galit <aroblesgalit@gmail.com>
2020-08-28 09:32:56 -0700

    Added a thunk that will make an AJAX call to retrieve a list of posts

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,5 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
+import { client } from '../../api/client';
 // import { sub } from 'date-fns';
 
 const initialState = {
@@ -6,6 +7,11 @@ const initialState = {
     status: 'idle',
     error: null
 };
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const response = await client.get('/fakeApi/posts');
+    return response.posts;
+});
 
 // [
 //     {
