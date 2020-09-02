17ea633903b9e92f9a523ea1abe8f84fd90b13f5
Alvin Galit <aroblesgalit@gmail.com>
2020-09-02 15:52:51 -0700

    Created component PostExcerpt

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -5,6 +5,7 @@ import { PostAuthor } from './PostAuthor';
 import { TimeAgo } from './TimeAgo';
 import { ReactionButtons } from './ReactionButtons';
 import { selectAllPosts, fetchPosts } from './postsSlice';
+import { PostExcerpt } from './PostExcerpt';
 
 export const PostsList = () => {
     const dispatch = useDispatch();
@@ -38,25 +39,25 @@ export const PostsList = () => {
     }
 
     // Sort posts in reverse chronological order by datetime string
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
+    // const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
 
-    const renderedPosts = orderedPosts.map(post => (
-        <article className='post-excerpt' key={post.id}>
-            <h3>{post.title}</h3>
-            <PostAuthor userId={post.user} />
-            <TimeAgo timestamp={post.date} />
-            <p>{post.content.substring(0, 100)}</p>
-            <ReactionButtons post={post} />
-            <Link to={`/posts/${post.id}`} className='button muted-button'>
-                View Post
-            </Link>
-        </article>
-    ));
+    // const renderedPosts = orderedPosts.map(post => (
+    //     <article className='post-excerpt' key={post.id}>
+    //         <h3>{post.title}</h3>
+    //         <PostAuthor userId={post.user} />
+    //         <TimeAgo timestamp={post.date} />
+    //         <p>{post.content.substring(0, 100)}</p>
+    //         <ReactionButtons post={post} />
+    //         <Link to={`/posts/${post.id}`} className='button muted-button'>
+    //             View Post
+    //         </Link>
+    //     </article>
+    // ));
 
     return (
-        <section>
+        <section className='posts-list'>
             <h2>Posts</h2>
-            {renderedPosts}
+            {content}
         </section>
     );
 };
