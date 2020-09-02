d6263e68cbe10d949b32180f7f91acd57639060b
Alvin Galit <aroblesgalit@gmail.com>
2020-09-01 22:35:41 -0700

    Updated PostsList to show a different bit of UI based on the state.posts.status

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -12,12 +12,30 @@ export const PostsList = () => {
     const posts = useSelector(selectAllPosts);
 
     const postStatus = useSelector(state => state.posts.status);
+    const error = useSelector(state => state.posts.error);
 
     useEffect(() => {
         if (postStatus === 'idle') {
             dispatch(fetchPosts())
         }
     }, [postStatus, dispatch])
+
+    let content;
+
+    if (postStatus === 'loading') {
+        content = <div className='loader'>Loading...</div>
+    } else if (postStatus === 'succeeded') {
+        // Sort posts in reverse chronological order by datetime string
+        const orderedPosts = posts
+            .slice()
+            .sort((a, b) => b.date.localeCompare(a.date));
+
+        content = orderedPosts.map(post => (
+            <PostExcerpt key={post.id} post={post} />
+        ))
+    } else if (postStatus === 'failed') {
+        content = <div>{error}</div>
+    }
 
     // Sort posts in reverse chronological order by datetime string
     const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
